7de898b477315962cf0fa6e4fae9b09e98491cac
Paramveer <paramveermarwahafc@gmail.com>
2025-08-23 20:25:29 +0200

    feat: add playerNames state, activePlayer, boardGame, and log of moves, check winner and draw

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,51 @@
 import { useState } from "react";
+import { initialBoardGame } from "./utils/gameData";
+import { checkWinner } from "./utils/checkWinner";
 
 function App() {
+  const [playerNames, setPlayerNames] = useState({
+    X: "player 1",
+    O: "player 2",
+  });
+
+  const [log, setLog] = useState([]);
+  const [activePlayer, setActivePlayer] = useState("X");
+  const [boardGame, setBoardGame] = useState(initialBoardGame);
+
+  // Derived state
+  const winner = checkWinner(boardGame);
+  const isDraw = boardGame.flat().every((cell) => cell !== null);
+
+  function handleRestart() {
+    setBoardGame(initialBoardGame);
+    setLog([]);
+    setActivePlayer("X");
+  }
+
+  // update logs
+  setLog((prev) => {
+    return [{ player: activePlayer, row: rowIndex, column: colIndex }, ...prev];
+  });
+
+  // set active player
+  setActivePlayer((prev) => {
+    return prev === "X" ? "O" : "X";
+  });
+
+  // update board
+  const updatedBoard = boardGame.map((row) => [...row]); // Deep Copy
+  updatedBoard[rowIndex][colIndex] = activePlayer;
+  setBoardGame(updatedBoard);
+}
+
+function handlePlayerName(symbol, playerName) {
+  setPlayerNames((prev) => {
+    return {
+      ...prev,
+      [symbol]: playerName,
+    };
+  });
+
   return <></>;
 }
 
